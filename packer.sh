9e99fb3121033a8642801831d93a632dca90f19d
Faisal Afzal <afzal.faisal@gmail.com>
2022-06-30 09:31:54 -0400

    Updated for base build

--- packer.sh
+++ packer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-packer build -var-file="faisal.pkrvars.hcl" .
+packer_command=${packer_command:-validate}
+packer_var_file=${packer_var_file:-faisal.pkrvars.hcl}
+
+echo "INFO: Running packer ${packer_command}."
+
+packer ${packer_command} -var-file=${packer_var_file} .
